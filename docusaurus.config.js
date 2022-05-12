4c60f03312eccca37ffbf339a6d7fdff68043807
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-12 22:46:15 +0800

    feat: add math & katex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -38,6 +40,8 @@ const config = {
           path: './docs',
           lastVersion: 'current',
           onlyIncludeVersions: ['current'],
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
 
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -56,6 +60,16 @@ const config = {
         },
       }),
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
@@ -136,6 +150,8 @@ const config = {
           path: 'solutions',
           routeBasePath: 'solutions',
           sidebarPath: require.resolve('./sidebars.js'),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         }, 
       ],
       [
@@ -145,6 +161,8 @@ const config = {
           path: 'tutorials',
           routeBasePath: 'tutorials',
           sidebarPath: require.resolve('./sidebars.js'),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         }, 
       ],
   ],
