0da39db77e4068809c9271e0353d207bf5ccb65e
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-13 22:39:13 +0800

    feat: add edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,18 +44,13 @@ const config = {
           rehypePlugins: [katex],
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,
-
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wingkwong/leetcode-the-hard-way',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wingkwong/leetcode-the-hard-way',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -155,6 +150,8 @@ const config = {
           rehypePlugins: [katex],
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,
+          editUrl:
+            'https://github.com/wingkwong/leetcode-the-hard-way',
         }, 
       ],
       [
@@ -168,6 +165,8 @@ const config = {
           rehypePlugins: [katex],
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,
+          editUrl:
+            'https://github.com/wingkwong/leetcode-the-hard-way',
         }, 
       ],
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -13,13 +12,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -29,8 +21,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="LeetCode The Hard Way - From Absolute Beginner to Quitter">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
