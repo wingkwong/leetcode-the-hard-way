961d6417083267b8b5f4329758c49d6c8e08c61f
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-14 23:53:09 +0800

    fix: github page deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,9 +17,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wingkwong',
+  projectName: 'leetcode-the-hard-way',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
