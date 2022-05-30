c17bc29c5393ad852c8e0721741c300c053b7441
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-30 10:59:08 +0800

    feat: sort by difficulty

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 export default function Table({title, data}) {
-  const renderRow = data.map(d => {
+  const sortOrder = {
+    'Easy': 0,
+    'Medium': 1,
+    'Hard': 2
+  }
+  const renderRow = data.sort((x, y) => sortOrder[x.difficulty] - sortOrder[y.difficulty]).map(d => {
     return (
       <tr>
         <td>
