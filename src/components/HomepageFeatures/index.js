9891a4d365dc0b752d23e268a9287889b50355f4
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-16 20:37:26 +0800

    feat: revise landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -36,9 +36,6 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        {/* <Svg className={styles.featureSvg} role="img" /> */}
-      </div>
-      <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
