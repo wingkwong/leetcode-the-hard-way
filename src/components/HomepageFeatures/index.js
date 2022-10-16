1426ce64e3d6c214536dd8ea7caa7927a93d952f
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-16 20:31:26 +0800

    chore: hide svg

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -36,7 +36,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
