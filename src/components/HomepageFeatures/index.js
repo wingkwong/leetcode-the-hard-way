ceb8b3ce543515cd394a33bb041a0e7f4885525f
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-16 20:22:39 +0800

    feat: revise landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
+    title: 'DSA Topics Tutorials',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Learn Data Structures and Algorithms ranged from basic to advanced via LeetCode Problems
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Line By Line Explanations',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Unlike most of posts in Discussion, we tend to provide detailed explanations via Line By Line fashion
+      </>
+    ),
+  },
+  {
+    title: 'LeetCode Community',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Join the Discord to discuss Leetcode Topics with the Community in real time
       </>
     ),
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-// import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
-  // const {siteConfig} = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
-      <img src="https://user-images.githubusercontent.com/35857179/168307611-1b28018b-fc98-4cff-88a6-d8312ac7bce6.png"/> 
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
     </header>
   );
 }
@@ -18,11 +21,11 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={`Hello from ${siteConfig.title}`}
       description="LeetCode The Hard Way - From Absolute Beginner to Quitter">
       <HomepageHeader />
       <main>
-        {/* <HomepageFeatures /> */}
+        <HomepageFeatures />
       </main>
     </Layout>
   );
