b08880f2ad6c4462c4c90610e91e16a60047bdc9
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-21 23:30:41 +0800

    feat: revamp landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
@@ -12,6 +13,27 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary"
+            to="/tutorials/category/basic-topics">
+            📗 Learn Basic Topics
+          </Link>
+          <Link
+            className="button button--secondary"
+            to="/solutions/category/0000---0099">
+            ✍️ Solution Explanations
+          </Link>
+          <span className={styles.gitHubButtonWrapper}>
+            <iframe
+              className={styles.gitHubButton}
+              src="https://ghbtns.com/github-btn.html?user=wingkwong&amp;repo=leetcode-the-hard-way&amp;type=star&amp;count=true&amp;size=large"
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
+        </div>
       </div>
     </header>
   );
