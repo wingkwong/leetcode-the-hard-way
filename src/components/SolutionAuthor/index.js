2a8826b829617dd9423e412690d18b2a002e2b89
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-23 14:58:08 +0800

    chore: revise code block author wordings

--- src/components/SolutionAuthor/index.js
+++ src/components/SolutionAuthor/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function SolutionAuthor({name}) {
   return (
     <div className="solution-author-wrapper">
-      <span>This solution is written by { name }</span>
+      <span>Written by { name }</span>
     </div>
   )};
