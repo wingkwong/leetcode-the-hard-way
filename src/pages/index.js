1a89fb2691b921ffc77d25f73d40185d16b97e73
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-21 22:48:21 +0800

    feat: revise landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
+    <section className={clsx('hero', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title"><span className={styles.highlightedColor}>{siteConfig.title}</span></h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -36,8 +36,39 @@ function HomepageHeader() {
           </span> */}
         </div>
       </div>
-    </header>
+    </section>
   );
+}
+
+function HomepageAbout() {
+  return (
+    <section className={styles.about}>
+      <div className="container">
+        <h1>📙 About</h1>
+        <p style={{textAlign: 'left'}}> 
+          Welcome to "LeetCode The Hard Way," the ultimate resource for those looking to improve their skills in data structures and algorithms! Our website is dedicated to providing comprehensive tutorials and detailed solutions to some of the most challenging problems in LeetCode. Whether you're a student looking to ace your next exam or a professional seeking to level up your coding skills, our tutorials and solutions are designed to help you succeed. Our content covers a range of popular programming languages, so you can learn and practice using the language of your choice.
+          <br/><br/>
+          From basic data structures to advanced topics, we've got you covered. At "LeetCode The Hard Way," we believe that learning should be fun and engaging. With plenty of LC problems and examples to work through, you'll have the opportunity to apply what you've learned and test your skills. So why wait? Start exploring our tutorials and solutions today, and take the first step towards becoming a DSA expert!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function HomepageAdvertise() {
+  return (
+    <section className={styles.advertise}>
+      <div className="container">
+        <h1>🤝 Sponsorship and Advertisement</h1>
+        <p> 
+          Interested in advertising on LeetCode The Hard Way? Our website provides free and comprehensive tutorials and solutions. As a free resource, we rely on the support of our community and advertisers to continue providing high-quality content. By sponsoring "LeetCode The Hard Way," you'll have the opportunity to connect with our audience through targeted advertisements and brand placement, while also supporting a valuable resource for programmers everywhere.
+        </p>
+        <p>
+          If you are interested in sponsoring and advertising with us, please email us at <a href="mailto:leetcodethehardway@gmail.com">here</a>.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default function Home() {
@@ -50,9 +81,11 @@ export default function Home() {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4531209581366540"
       crossorigin="anonymous"></script>
       </Head>
-      <HomepageHeader />
       <main>
+        <HomepageHeader />
         <HomepageFeatures />
+        <HomepageAbout />
+        <HomepageAdvertise />
       </main>
     </Layout>
   );
